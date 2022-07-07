812584a53a8ff431984400bfa348f256b10abe8e
Sona Jasani <jasanisona@gmail.com>
2022-07-06 22:17:17 -0400

    edit user backend route created

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import User
 from app.models import User, db
 from ..forms.edit_user_form import EditUserForm
+from app.aws3 import (upload_file_to_s3, allowed_file, get_unique_filename)
 
 user_routes = Blueprint('users', __name__)
 
@@ -44,9 +44,35 @@ def user(id):
                         # UPDATE A USER #
 ######################################################################################################
 
+@user_routes.route('/edit/<int:user_id>', methods=["PUT"])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    url = None
+    if user.id == int(user_id):
+        form = EditUserForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            # TODO AWS S3 Bucket Upload Start - profile_image_url
+            
+            if request.files:
+                image = request.files["profile_pic_url"]
+                if not allowed_file(image.filename):
+                    return {"errors": "file type not permitted"}, 400
+                image.filename = get_unique_filename(image.filename)
+                upload = upload_file_to_s3(image)
+                if "url" not in upload:
+                        return upload, 400
+                url = upload["url"]
+            # TODO AWS S3 Bucket Upload End - profile_image_url
 
+            user.username = form.data['username'],
+            user.email = form.data['email'],
+            user.bio = form.data['bio'],
+            user.profile_pic_url = None if url is None else url,
+            db.session.commit()
+            return user.to_dict(), 201
 
-
+    return {'errors': error_messages(form.errors)}, 400
 
 
 ######################################################################################################
