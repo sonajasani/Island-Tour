9f05132a78e6c0410db36117f727723064d95b8f
Sona Jasani <jasanisona@gmail.com>
2022-07-10 17:57:18 -0400

    loading page css done

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,25 +1,19 @@
-import { useHistory } from 'react-router-dom';
-import { useEffect } from 'react'
-import notFound from '../../images/airbnb-404.gif'
+import { NavLink } from 'react-router-dom';
+import notFound from '../../images/error-404.gif'
+import './PageNotFound.css'
 
 function PageNotFound() {
-  let history = useHistory();
 
-  useEffect(() => {
-    setTimeout(() => {
-      history.push('/');
-    }, 3000)
-  }, [])
+
 
   return (
     <div className="not-found-page">
       <div className="not-found-info">
-        <p className="not-found-header"> OOOOPS!</p>
-        <p>We can't seem to find the page you're looking for.</p>
-        <p>Don't worry, we'll take you back :) </p>
-        <p className='bold'>Error Code: 404</p>
+        <img src={notFound} alt="NotFound"></img>
+        <NavLink to='/' exact={true} className='page-not-found-link'>
+          Go To Home Page..!
+        </NavLink>
       </div>
-      <img src={notFound} alt="NotFound"></img>
     </div>
   )
 }
