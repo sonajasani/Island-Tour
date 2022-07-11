e723c197f599a04dc796cfb66ad9c839b66416e0
Sona Jasani <jasanisona@gmail.com>
2022-07-11 18:24:26 -0400

    all resort page css

--- app/api/utils.py
+++ app/api/utils.py
@@ -7,3 +7,4 @@ def validation_errors_to_error_messages(validation_errors):
         for error in validation_errors[field]:
             errorMessages.append(f'{field} : {error}')
     return errorMessages
+
